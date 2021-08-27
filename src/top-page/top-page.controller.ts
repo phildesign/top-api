2b45efd5ba0ca807915c0cf42c61781bf7c90726
Phil <phil.nice.23@gmail.com>
2021-08-27 20:58:54 +0300

    add environment

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -10,14 +10,22 @@ import {
   HttpCode,
 } from '@nestjs/common';
 import { TopPageModel } from './top-page.model';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService: ConfigService);
+
+  @Get('get/:alias')
+  async get(@Param('alias') alias: string): Promise<TopPageModel> {
+    this.configService.get('TEST');
+  }
+
   @Post('create')
   async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
 
-  @Get(':id')
-  async get(@Param('id') id: string) {}
+  // @Get(':id')
+  // async get(@Param('id') id: string) {}
 
   @Delete(':id')
   async delete(@Param('id') id: string) {}
